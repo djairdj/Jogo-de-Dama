d6334ffd0088ba5f9ecbec2cf6cf71fddf31ad59
Djair Alves <djair.dj@gmail.com>
2021-12-21 00:13:57 -0300

    Teste de uso do toString do Enum

--- src/Teste/TesteEnum.java
+++ src/Teste/TesteEnum.java
@@ -4,8 +4,8 @@ import Modelos.Pecas;
 
 public class TesteEnum {
     public static void main(String[] args) {
-        System.out.println(Pecas.PecaPreta.peca);
-        System.out.println(Pecas.PecaBranca.name());
+        System.out.println(Pecas.PecaPreta);
+        System.out.println(Pecas.PecaBranca);
         System.out.println(Pecas.DamaPreta);
         System.out.println(Pecas.DamaBranca);
     }
