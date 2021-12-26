f2513b542ae6c2923b240d0abde6583f83287689
Djair Alves <djair.dj@gmail.com>
2021-12-26 17:20:45 -0300

    Ajuste no teste

--- src/Teste/TesteJogador.java
+++ src/Teste/TesteJogador.java
@@ -3,12 +3,14 @@ package Teste;
 import Modelos.Jogador;
 import Modelos.Peca;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class TesteJogador {
     public static void main(String[] args) {
-        var pedra = Peca.values()[new Random().nextInt(Peca.values().length)];
-        Jogador j = new Jogador("Kelvin", pedra);
-        System.out.println(j.getPeca());
+        var pedra1 = Peca.values()[new Random().nextInt(Peca.values().length)];
+        var pedra2 = Peca.values()[new Random().nextInt(Peca.values().length)];
+        Jogador j = new Jogador("Kelvin", pedra1, pedra2);
+        System.out.println(Arrays.toString(j.getPecas()));
     }
 }
