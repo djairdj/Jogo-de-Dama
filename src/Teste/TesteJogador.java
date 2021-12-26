e0a1aafab3ef2b9da5783774ce329055159b25b4
Djair Alves <djair.dj@gmail.com>
2021-12-26 17:51:06 -0300

    Melhorando o teste da exibição das informações do jogador

--- src/Teste/TesteJogador.java
+++ src/Teste/TesteJogador.java
@@ -3,14 +3,10 @@ package Teste;
 import Modelos.Jogador;
 import Modelos.Peca;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class TesteJogador {
     public static void main(String[] args) {
-        var pedra1 = Peca.values()[new Random().nextInt(Peca.values().length)];
-        var pedra2 = Peca.values()[new Random().nextInt(Peca.values().length)];
-        Jogador j = new Jogador("Kelvin", pedra1, pedra2);
-        System.out.println(Arrays.toString(j.getPecas()));
+        int sorteio = (int) Math.round(Math.random()) % 2;
+        Jogador j = new Jogador("Kelvin", Peca.values()[sorteio], Peca.values()[sorteio + 2]);
+        System.out.println(j);
     }
 }
