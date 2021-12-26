0aaceac19258800077b43a6fdb9317dd1e18481b
Djair Alves <djair.dj@gmail.com>
2021-12-26 18:28:37 -0300

    Testando o método toString do Jogo

--- src/Teste/TesteJogo.java
+++ src/Teste/TesteJogo.java
@@ -4,10 +4,8 @@ import Modelos.Jogo;
 
 public class TesteJogo {
     public static void main(String[] args) {
-        System.out.println("Usando método estático.");
-        Jogo.novoJogo();
-        System.out.println("Usando método do objeto.");
         Jogo h = new Jogo();
         h.jogar();
+        System.out.println(h);
     }
 }
