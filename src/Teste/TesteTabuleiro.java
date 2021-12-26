af28754335c8f39f3c9c537c458c2abdb82a2627
Djair Alves <djair.dj@gmail.com>
2021-12-26 15:23:07 -0300

    Ajustes no teste do tabuleiro

--- src/Teste/TesteTabuleiro.java
+++ src/Teste/TesteTabuleiro.java
@@ -6,7 +6,6 @@ import Modelos.Tabuleiro;
 public class TesteTabuleiro {
     public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro(Peca.DamaBranca.peca, Peca.DamaPreta.peca);
-        tabuleiro.zeraTabuleiro();
         System.out.print(tabuleiro);
     }
 }
